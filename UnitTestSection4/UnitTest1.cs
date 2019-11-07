7dbfbd87c6ba23de62bbc895a09ed44f4f91487a
roman svitukha <griind@gmail.com>
2019-11-06 20:23:24 -0600

    Add project files.

---
+++ UnitTestSection4/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestSection4
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
